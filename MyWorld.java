8bef06b076fc8ab5bd2f471192a73f400d7b6ece
MonkeyShock3000 <167050968+MonkeyShock3000@users.noreply.github.com>
2024-05-14 17:11:37 -0400

    Finished part #1
    - Changed Apple
    - Changed Elephant
    - Added and Changed Label
    - Changed MyWorld

--- MyWorld.java
+++ MyWorld.java
@@ -8,7 +8,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-
+    public int score = 0;
+    Label scoreLabel;
+    
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -16,12 +18,44 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1);
+        super(600, 400, 1, false);
         
+        //Create the elephant object
         Elephant elephant = new Elephant();
-        addObject(elephant, 300, 200);
+        addObject(elephant, 300, 300);
         
+        // Create a label
+        scoreLabel = new Label("", 80);
+        addObject(scoreLabel, 20, 20);
+        
+        createApple();
+    }
+    
+    /**
+     * End the game and draw 'GameOver'
+     */
+    public void gameOver()
+    {
+        Label gameOverLabel = new Label("Game Over", 100);
+        addObject(gameOverLabel, 300, 200);
+    }
+    
+    /**
+     * Increase score
+     */
+    public void increaseScore()
+    {
+        score++;
+        scoreLabel.setValue(score);
+    }
+    /**
+     * Create a new apple at random location at top of screen
+     */
+    public void createApple()
+    {
         Apple apple = new Apple();
-        addObject(apple, 300, 0);
+        int x = Greenfoot.getRandomNumber(600);
+        int y = 0;
+        addObject(apple, x, y);
     }
 }
