5221bb48d0bd1a157b928b85b97141c59ee58221
MonkeyShock3000 <167050968+MonkeyShock3000@users.noreply.github.com>
2024-05-20 16:13:00 -0400

    Test (Greenfoot crashed)
    Animating images

--- Elephant.java
+++ Elephant.java
@@ -8,11 +8,32 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Elephant extends Actor
 {
+    GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
+    GreenfootImage[] idle = new GreenfootImage[8];
+    
     /**
-     * Act - do whatever the Elephant wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
+     * Constructor - The code that gets run one time when object is created
      */
-    public void act()
+    
+public Elephant()
+{
+    for(int i = 0; i < idle.length; i++)
+    {
+         idle[i] = new GreenfootImage("images/elephant_idle/idle" + i + " .png");   
+    }
+    setImage(idle[0]);
+}
+    
+/**
+ * Animate the elephant
+ */    
+int imageIndex = 0;
+public void animateElephant()
+{
+    setImage(idle[imageIndex]);
+    imageIndex = imageIndex + 1 % idle.length;
+}
+public void act()
     {
         // Add your action code here.
         if(Greenfoot.isKeyDown("left"))
@@ -27,7 +48,9 @@ public class Elephant extends Actor
         
         //Remove apple if elephant eats it
         eat();
-
+        
+        // Animate the elephant
+        animateElephant();
     }
     
     /**
@@ -41,6 +64,7 @@ public class Elephant extends Actor
            MyWorld world = (MyWorld) getWorld();
            world.createApple();
            world.increaseScore();
+           elephantSound.play();
         }
     }
 }
